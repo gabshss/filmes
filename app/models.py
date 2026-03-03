fc412d070fb4063c6d0573f2fc1eee9969bd55b2
gabi <you@example.com>
2026-03-03 09:11:52 -0300

    final models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
- class Continente(models.Model):
+class Continente(models.Model):
     nome = models.CharField(max_length=100, verbose_name = "Continente")
 
     def __str__(self):
@@ -11,7 +11,7 @@ from django.db import models
 
 class Pais(models.Model):
     nome = models.CharField(max_length=100, verbose_name = "País")
-    continente = ForeignKey(Continente, on-delete = models.CASCADE)
+    continente = models.ForeignKey(Continente, on_delete = models.CASCADE,verbose_name="Continente")
 
     def __str__(self):
         return self.nome
@@ -25,6 +25,85 @@ class Genero(models.Model):
 
     def __str__(self):
         return self.nome 
+
+    class Meta:
+        verbose_name = "Gênero"
+        verbose_name_plural = "Gêneros"
+
+class Pessoa(models.Model):
+    cargo = models.CharField(max_length=100, verbose_name = "Cargo")
+    nome = models.CharField(max_length=100, verbose_name = "Nome")
+    insta = models.CharField(max_length=100, verbose_name = "Instagram")
+    nacionalidade = models.CharField(max_length=100, verbose_name = "Nacionalidade")
+    
+    def __str__(self):
+        return f"{self.nome} ({self.cargo})"
+
+    class Meta:
+        verbose_name = "Pessoa"
+        verbose_name_plural = "Pessoas"
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100,verbose_name = "Título")
+    duracao = models.CharField(max_length=100,verbose_name = "Duração")
+    sinopse = models.TextField(verbose_name = "Sinopse")
+    data = models.DateField(verbose_name = "Data de lançamento")
+    nota = models.DecimalField(max_digits=3, decimal_places=1, verbose_name = "Nota de avaliação")
+    genero = models.ForeignKey(Genero, on_delete = models.CASCADE, verbose_name = "Gênero")
+    pais = models.ForeignKey(Pais, on_delete = models.CASCADE, verbose_name ="País")
+    diretor = models.ForeignKey(Pessoa, on_delete = models.CASCADE, verbose_name = "Diretor")
+    atores = models.ManyToManyField(Pessoa, related_name="filmes_como_ator", verbose_name="Atores", blank=True,)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name = "Filme"
+        verbose_name_plural = "Filmes"
+        
+class Serie(models.Model):
+    titulo = models.CharField(max_length=100,verbose_name = "Título")
+    duracao = models.CharField(max_length=100,verbose_name = "Duração")
+    sinopse = models.TextField(verbose_name = "Sinopse")
+    data = models.DateField(verbose_name = "Data de lançamento")
+    nota = models.DecimalField(max_digits=3, decimal_places=1, verbose_name = "Nota de avaliação")
+    genero = models.ForeignKey(Genero, on_delete = models.CASCADE, verbose_name = "Gênero")
+    pais = models.ForeignKey(Pais, on_delete = models.CASCADE, verbose_name ="País")
+    diretor = models.ForeignKey(Pessoa, on_delete = models.CASCADE, verbose_name = "Diretor")
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name = "Série"
+        verbose_name_plural = "Séries"
+
+class Temporada(models.Model):
+    serie = models.ForeignKey(Serie, on_delete=models.CASCADE, related_name="temporadas", verbose_name="Série")
+    numero = models.IntegerField(verbose_name="Número da temporada")
+
+    def __str__(self):
+        return f"{self.serie.titulo} - {self.numero}ª Temporada"
+
+    class Meta:
+        verbose_name = "Temporada"
+        verbose_name_plural = "Temporadas"
+        unique_together = ("serie", "numero")
+
+class Episodio(models.Model):
+    nome = models.CharField(max_length=100, verbose_name = "Título")
+    temporada = models.ForeignKey(Temporada, on_delete = models.CASCADE, verbose_name = "Temporada")
+    duracao = models.CharField(max_length=100, verbose_name = "Duração")
+    data = models.DateField(verbose_name = "Data de lançamento")
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Episódio"
+        verbose_name_plural = "Episódios"
+
+
 
 
 
