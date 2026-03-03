99f76c79af9ce4dbdd750c454b1b0f21bd8b0352
gabi <you@example.com>
2026-03-03 10:57:06 -0300

    final models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Continente)
+admin.site.register(Pais)
+admin.site.register(Genero)
+admin.site.register(Pessoa)
+admin.site.register(Filme)
+admin.site.register(Serie)
+admin.site.register(Temporada)
+admin.site.register(Episodio)
