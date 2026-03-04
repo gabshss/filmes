d25647f3632b971a7437c7f4370a851838f3efb2
gabi <you@example.com>
2026-03-04 10:57:44 -0300

    admin

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,47 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Continente)
-admin.site.register(Pais)
-admin.site.register(Genero)
-admin.site.register(Pessoa)
-admin.site.register(Filme)
-admin.site.register(Serie)
-admin.site.register(Temporada)
-admin.site.register(Episodio)
+@admin.register(Continente)
+class ContinenteAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Pais)
+class PaisAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'continente')
+    list_filter = ('continente',)
+    search_fields = ('nome',)
+
+@admin.register(Genero)
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'nacionalidade')
+    list_filter = ('cargo','nacionalidade')
+    search_fields = ('nome', 'cargo', 'nacionalidade')  
+
+@admin.register(Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'genero', 'pais', 'diretor', 'data')
+    list_filter = ('genero','pais','data')
+    search_fields = ('titulo', 'diretor__nome', 'pais')
+
+@admin.register(Serie)
+class SerieAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'duracao', 'genero', 'diretor')
+    list_filter = ('nota',)
+    search_fields = ('titulo', 'diretor', 'pais')
+    
+@admin.register(Temporada)
+class TemporadaAdmin(admin.ModelAdmin):
+    list_display = ('serie', 'numero')
+
+
+@admin.register(Episodio)
+class EpisodioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'temporada', 'duracao', 'data')
+    list_filter =  ('temporada',)
+    search_fields = ('nome',)
